5dc4ccbcce5e6c85870da296c4999656db4d6bbd
yasaman <hbn.yasaman@gmail.com>
2020-04-29 19:54:39 +0430

    complete homework one

--- HW1/src/Main.java
+++ HW1/src/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     DFA dfa = new DFA();
+    //read data from file
     dfa.readData();
+    ///get string
         Scanner sc = new Scanner(System.in);
         String st = sc.next();
         System.out.println(dfa.checkString(st));
--- HW1/src/machine/DFA.java
+++ HW1/src/machine/DFA.java
@@ -1,9 +1,15 @@
 package machine;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
+/**
+ * this class read data from file and check whether string accepted or not
+ * @author Yasaman Haghbin
+ * @since 2020
+ * @version 1.0
+ */
 public class DFA {
     private String[] alphabet;
     private String[] state;
@@ -15,23 +21,32 @@ public class DFA {
         stateChangers = new StateChanger[1000];
     }
 
+    /**
+     * readData method read data from file and determine start state, final state, alphabet and states;
+     * @throws IOException if doesn't find file or can't find it
+     */
     public void readData() {
         File file = new File("E:\\university\\theory of machines and languages\\HW1\\dfa.txt");
         int counter = 0;
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
+            //read four first line for getting alphabet and states;
             for (int i = 0; i < 4; i++) {
                 st = br.readLine();
                 if (i == 0)
+                    //split alphabet and push them in array
                     alphabet = st.split(" ");
                 else if (i == 1)
+                    //split state and put them in array
                     state = st.split(" ");
                 else if (i == 2)
                     startState = st;
                 else
                     finiteState = st.split(" ");
             }
+            /**read remaining data and transform functions and create a stateChanger object
+            for each function and store it in stateChangers array**/
             while ((st = br.readLine()) != null) {
                 String[] s = st.split(" ");
                 StateChanger stCh = new StateChanger(s[0], s[1].charAt(0), s[2]);
@@ -53,12 +68,18 @@ public class DFA {
         for (String i : finiteState)
             System.out.println("finiteState  " + i);
     }
-
+      /**
+       * checkString method check whether myString accepted with dfa or not
+        **/
     public Boolean checkString(String myString) {
         boolean check;
+        //read ol the string
         for (int i = 0; i < myString.length(); i++) {
+            //this boolean is for checking whether string can continue or it isn't accepted
              check = false;
+             //read all the transform function
             for (int j=0;j<=stateCounter;j++){
+                //if string can accepted with one of the state,I change the startState
                 if (stateChangers[j].getFirstState().equals(startState)) {
                     if (stateChangers[j].getAlphabet() == myString.charAt(i)) {
                         startState = stateChangers[j].getEndState();
@@ -71,6 +92,7 @@ public class DFA {
                 return false;
             }
         }
+        //if machine is on finite state,it accepted, else failed
         boolean checkResult= false;
         for (String s : finiteState) {
             if (startState.equals(s)) {
